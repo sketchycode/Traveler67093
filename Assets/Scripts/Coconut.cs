9c492357c3070a5088cd3bdf578f4b8ff21b47e3
Amelia <ladyvipaka@yahoo.com>
2018-01-26 21:42:56 -0700

    Bounce that coconut.

---
+++ Assets/Scripts/Coconut.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerInteractable))]
+public class Coconut : MonoBehaviour
+{
+    private void Start()
+    {
+        PlayerInteractable coconutinteractive = GetComponent<PlayerInteractable>();
+        coconutinteractive.PlayerInteracted += Coconutinteractive_PlayerInteracted;
+        Rigidbody rigidcoconut = GetComponent<Rigidbody>();
+        rigidcoconut.isKinematic = true;
+    }
+
+    private void Coconutinteractive_PlayerInteracted(object sender, System.EventArgs e)
+    {
+        Rigidbody bouncycoconut = GetComponent<Rigidbody>();
+        bouncycoconut.isKinematic = false;
+    }
+
+    void Update()
+    {
+
+    }
+}
