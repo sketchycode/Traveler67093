c1a6adc2cfc52ae1f281ab2c4bbe67d41d80024a
Amelia <ladyvipaka@yahoo.com>
2018-01-27 10:39:47 -0700

    Proximity for portal and portal class

---
+++ Assets/Scripts/PlayerProximity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProximity : MonoBehaviour
+{
+    public event EventHandler PlayerProximal;
+
+    public void ActivatePortal()
+    {
+        Debug.Log("proximal to something");
+        if (PlayerProximal != null)
+        {
+            PlayerProximal(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerProximity))]
+public class Portal : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
