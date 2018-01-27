8c0dab92f9b4cd17433b5628ea253aef25869cf2
Amelia <ladyvipaka@yahoo.com>
2018-01-27 10:47:33 -0700

    Portal active and inactive states methods

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,9 +7,19 @@ public class Portal : MonoBehaviour
 {
     private void Start()
     {
+        PlayerProximity portalInteractive = GetComponent<PlayerProximity>();
+        portalInteractive.PlayerProximal += InactivatePortal_PlayerProximal;
     }
 
     void Update()
+    {
+    }
+
+    private void InactivatePortal_PlayerProximal(object sender, System.EventArgs e)
+    {
+    }
+
+    private void ActivatePortal_PlayerProximal(object sender, System.EventArgs e)
     {
     }
 }
