1e4e1b7378b1e46eabe9bfc8d06b869f13876c68
Amelia <ladyvipaka@yahoo.com>
2018-01-27 20:27:58 -0700

    New assets and glyph code changes

--- Assets/Scripts/Glyph.cs
+++ Assets/Scripts/Glyph.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 [RequireComponent(typeof(Portal))]
 public class Glyph : MonoBehaviour
 {
+    public Transform target;
+
     private void Start()
     {
         PlayerInteractable glyphinteractive = GetComponent<PlayerInteractable>();
         glyphinteractive.PlayerInteracted += Glyphinteractive_PlayerInteracted;
+        MeshRenderer visibleTranslation = GetComponent<MeshRenderer>();
+        visibleTranslation.enabled = false;
     }
 
     private void Glyphinteractive_PlayerInteracted(object sender, System.EventArgs e)
     {
-        Debug.Log("test glyph");
+        Debug.Log("test stuff");
+        MeshRenderer visibleTranslation = GetComponent<MeshRenderer>();
+        visibleTranslation.enabled = true;
+        transform.LookAt(target);
     }
 
     void Update()
