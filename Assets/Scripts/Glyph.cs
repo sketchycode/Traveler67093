c830b0fd4f10f4e4604ef6b0eba0bed7a7d132dc
Amelia <ladyvipaka@yahoo.com>
2018-01-28 10:52:59 -0700

    More sounds and glyph list

--- Assets/Scripts/Glyph.cs
+++ Assets/Scripts/Glyph.cs
@@ -8,6 +8,8 @@ public class Glyph : MonoBehaviour
 {
     public Transform target;
     //sdouble[] GlyphAry = { "Translation1", "Translation2", "Translation3" };
+    List<string> TranslationAry = new List<string>(new string[] { "Translation1", "Translation2", "Translation3", "Translation4", "Translation5", "Translation6", "Translation8", "Translation10", "Translation12", "Translation15", "Translation18" });
+    List<string> GlyphAry = new List<string>(new string[] { "Glyph1", "Glyph2", "Glyph3", "Glyph4", "Glyph5", "Glyph6", "Glyph8", "Glyph10" });
 
     private void Start()
     {
