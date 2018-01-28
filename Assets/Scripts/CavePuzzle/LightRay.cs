69fd1d8ce70fc98a369b9918502a1dbf1512733e
jeremiah.nunn <jeremy@sketchyventures.com>
2018-01-27 21:35:19 -0700

    some lazer stuff

---
+++ Assets/Scripts/CavePuzzle/LightRay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightRay : MonoBehaviour {
+    public float radius;
+
+    private Transform cylinderTransform;
+    void Start()
+    {
+        cylinderTransform = GetComponentInChildren<Transform>();
+    }
+
+    void Update()
+    {
+        cylinderTransform.localScale = new Vector3(radius, radius, radius);
+    }
+}
